ab5e200964a0b62aac51afc880b24c084a69740d
Nossila-usb <minimonik1909@gmail.com>
2024-08-28 17:06:29 -0300

    versao 1.0

--- app30.py
+++ app30.py
@@ -2,17 +2,18 @@
 import pyautogui as aut
 import time
 
-link_reposi = input("Digite o link do repositório: ")
-versao = input("Digite o nome da versão: ")
-# tempo que cada comando demora para executar
+link_reposi = input('O link do repositório: ')
+versao = input('O nome da versão: ')
+
 aut.PAUSE = 1
-# instruções
+
+
 aut.press('win')
 aut.write('vscode')
 aut.press('enter')
-# espera 10 segundos para abrir o vscode e continuar com os comandos
+
 time.sleep(5)
-# continua as instruções
+
 aut.hotkey('ctrl', 'j')
 time.sleep(5)
 
@@ -22,9 +23,11 @@ aut.write('git add .')
 aut.press('enter')
 aut.write(f'git commit -m "{versao}"')
 aut.press('enter')
-# espera 5 segundos para dar tempo de fazer o commit
+
+
+
 time.sleep(5)
-# continua as instruções
+
 aut.write('git branch -M main')
 aut.press('enter')
 aut.write(f'git remote add origin {link_reposi}')
